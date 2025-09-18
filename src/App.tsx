d21f471236d241055d6d22e8b91d2e69398bfb86
Cosmas <alexisstar8@gmail.com>
2025-09-18 04:20:20 +0000

    Fix build: remove speed insights and update service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { ProtectedRoute } from '@/components/ProtectedRoute'
 import { AdminRoute } from '@/components/AdminRoute'
@@ -68,7 +67,7 @@ function App() {
               </Routes>
             </div>
           </Router>
-          <SpeedInsights />
+
         </ToastProvider>
       </AuthProvider>
     </QueryClientProvider>
